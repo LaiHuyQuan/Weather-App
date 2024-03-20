2952b3c92a7857ab072709ba4173a8cd5ff867ec
unknown <laiquan163@gmail.com>
2024-03-20 22:36:34 +0700

    update 20.3

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,47 +8,63 @@ var wind = document.querySelector('.wind span')
 var sun = document.querySelector('.sun span')
 var time = document.querySelector('.time')
 
-async function changeWeatherUI(){
+const weather = document.querySelector('.weather')
+const weatherContainer = document.querySelector('.weather-container')
+
+async function changeWeatherUI(defaultName) {
     let capitalSearch = search.value.trim()
-    let apiURL = 'https://api.openweathermap.org/data/2.5/weather?q='+capitalSearch+'&units=metric&appid=d78fd1588e1b7c0c2813576ba183a667'
-    
-    let data = await fetch(apiURL).then(res=>res.json())
+    let apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + capitalSearch + '&units=metric&appid=d78fd1588e1b7c0c2813576ba183a667'
+    let data = await fetch(apiURL).then(res => res.json())
     console.log(data)
-    
-    if (data.cod == 200){
-    city.innerText = data.name 
-    country.innerText = data.sys.country
-    visibility.innerText = data.visibility + 'm'
-    wind.innerText = data.wind.speed +'m/s'
-    sun.innerText = data.main.humidity +'%'
-    value.innerText = (data.main.temp) 
-    shortDesc.innerText = data.weather[0].main
-    time.innerText = new Date().toLocaleDateString('vi') 
+
+    if (data.cod == 200) {
+        city.innerText = data.name
+        country.innerText = data.sys.country
+        visibility.innerText = data.visibility + 'm'
+        wind.innerText = data.wind.speed + 'm/s'
+        sun.innerText = data.main.humidity + '%'
+        value.innerText = (data.main.temp)
+        shortDesc.innerText = data.weather[0].main
+        time.innerText = new Date().toLocaleDateString('vi')
     }else{
-        capitalSearch = "ha noi"
+        let apiURL = 'https://api.openweathermap.org/data/2.5/weather?q='+ defaultName+'&units=metric&appid=d78fd1588e1b7c0c2813576ba183a667'
+        let data = await fetch(apiURL).then(res => res.json())
+        city.innerText = data.name
+        country.innerText = data.sys.country
+        visibility.innerText = data.visibility + 'm'
+        wind.innerText = data.wind.speed + 'm/s'
+        sun.innerText = data.main.humidity + '%'
+        value.innerText = (data.main.temp)
+        shortDesc.innerText = data.weather[0].main
+        time.innerText = new Date().toLocaleDateString('vi')
     }
+    changeBackground();
+    
 }
 
 
-search.addEventListener('keypress',function(e){
-    if(e.code === 'Enter'){
-        changeWeatherUI('AAAAAA')
+search.addEventListener('keypress', function (e) {
+    if (e.code === 'Enter') {
+        changeWeatherUI('hanoi')
     }
 })
 
-// Change background
-const weather = document.querySelector('.weather')
-const body = document.querySelector('wrapper')
-
-if(value.innerText >= 25) {
-    weather.classList.add('hot')
-    body.classList.add('hot')
-    weather.classList.remove('cold')
-    body.classList.remove('cold')
-}else{
-    weather.classList.add('cold')
-    body.classList.add('cold')
-    weather.classList.remove('hot')
-    body.classList.remove('hot')
-
+console.log(value.innerText)
+function changeBackground() {
+    if (value.innerText >= 25) {
+        weather.classList.add('hot')
+        weatherContainer.classList.add('hot')
+        weather.classList.remove('cold')
+        weatherContainer.classList.remove('cold')
+        console.log("1")
+    } else {
+        weather.classList.add('cold')
+        weatherContainer.classList.add('cold')
+        weather.classList.remove('hot')
+        weatherContainer.classList.remove('hot')
+        console.log("2")
+    }
 }
+
+changeWeatherUI('hanoi')
+changeBackground();
